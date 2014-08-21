12df32335defeb57b0ef86601f9dba051756be27
Barry Belmont <belmont@umich.edu>
2014-08-21 11:57:26 -0400

    Good idea for overall arm/leg analysis

--- manualAnalysisDriveP2P.m
+++ manualAnalysisDriveP2P.m
@@ -1,7 +1,8 @@
+clear all; close all; clc
+%%
 [bioimpedanceFile, bioimpedancePath] = uigetfile('*.*','Pick an ACQ file');
 acq = load_acq(strcat(bioimpedancePath,bioimpedanceFile));
 
-%%
 ohmsPerVolt = 20;
 
 bArm = acq.data(:,1)*ohmsPerVolt;
@@ -51,7 +52,8 @@ bArmSMOOTH = smooth(bArmSMOOTH, Fs/10);
 respArmSMOOTH = smooth(bArmSMOOTH, Fs);
 cardArmSMOOTH = smooth(bArmSMOOTH - respArmSMOOTH);
 
-legData = [bLegSMOOTH, respLegSMOOTH, cardLegSMOOTH];
+legData = [bLegSMOOTH, respLegSMOOTH, cardLegSMOOTH, ...
+    bArmSMOOTH, respArmSMOOTH, cardArmSMOOTH];
 
 for indMarker = 1:size(timeMarkerBioimpedance,2)-1;    
     % The time of the region of interest
@@ -59,27 +61,16 @@ for indMarker = 1:size(timeMarkerBioimpedance,2)-1;
     tEnd = timeMarkerBioimpedanceInd(indMarker)+Fs*totalTime;
     
     % Signal during the time of interest (manuever)
-    legROI = bLegSMOOTH(tBegin:tEnd) - mean(bLegSMOOTH(tBegin:tEnd));
-    respLegROI = respLegSMOOTH(tBegin:tEnd) - mean(respLegSMOOTH(tBegin:tEnd));
-    cardLegROI = cardLegSMOOTH(tBegin:tEnd) - mean(cardLegSMOOTH(tBegin:tEnd));
-    armROI = bArmSMOOTH(tBegin:tEnd) - mean(bArmSMOOTH(tBegin:tEnd));
-    respArmROI = respArmSMOOTH(tBegin:tEnd) - mean(respArmSMOOTH(tBegin:tEnd));
-    cardArmROI = cardArmSMOOTH(tBegin:tEnd) - mean(cardArmSMOOTH(tBegin:tEnd));
+    for indDataType = 1:size(legData,2)
+        maxOverall(indMarker,indDataType) = ...
+            max(legData(tBegin:tEnd,indDataType));
+        minOverall(indMarker,indDataType) = ...
+            min(legData(tBegin:tEnd,indDataType));
+        dZOverall(indMarker,indDataType) = ...
+            maxOverall(indMarker,indDataType) - minOverall(indMarker,indDataType);
+    end
     
-    % Overall signal changes
-    [legMax, indLegMax] = findpeaks(legROI,'MINPEAKDISTANCE',Fs/2);
-    [armMax, indArmMax] = findpeaks(armROI,'MINPEAKDISTANCE',Fs/2);
-    legROIinv = 1.01*max(legROI) - legROI;
-    armROIinv = 1.01*max(armROI) - armROI;
-    [legMin, indLegMin] = findpeaks(legROIinv,'MINPEAKDISTANCE',Fs/2);
-    [armMin, indArmMin] = findpeaks(armROIinv,'MINPEAKDISTANCE',Fs/2);
-    
-    maxOverallLeg = max(legROI); maxOverallArm = max(armROI);
-    minOverallLeg = min(legROI); minOverallArm = min(armROI);
-    
-    dZOverallLeg(indMarker) = maxOverallLeg - minOverallLeg;
-    dZOverallArm(indMarker) = maxOverallArm - minOverallArm;
-    
+    acq.markers.szText{1,indMarker+1}(12:end)
     
     
 %     figure, plot(time', legROI,time(indMaxLeg),maximaLeg,'g*',time(indMinL),legROI(indMinL),'ro')
