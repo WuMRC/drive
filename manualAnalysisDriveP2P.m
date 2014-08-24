d7103d7cfc7fa34296208fc96fe8b8cef535d5ae
Barry Belmont <belmont@umich.edu>
2014-08-24 18:07:29 -0400

    Minor changes
    I’ve been running this code all day. My eyes are watering. I’m going to
    commit and step outside.

--- manualAnalysisDriveP2P.m
+++ manualAnalysisDriveP2P.m
@@ -1,13 +1,17 @@
 clear all; close all; clc
 %%
+clear NAMES dZOverall
 [bioimpedanceFile, bioimpedancePath] = uigetfile('*.*','Pick an ACQ file');
 acq = load_acq(strcat(bioimpedancePath,bioimpedanceFile));
+
+cd(bioimpedancePath);
 
 ohmsPerVolt = 20;
 
 bArm = acq.data(:,1)*ohmsPerVolt;
 bLeg = acq.data(:,2)*ohmsPerVolt;
-ppg = acq.data(:,3);
+ppg = acq.data(:,3); 
+
 
 dtBioimpedance = acq.hdr.graph.sample_time;
 timeStartBioimpedance = acq.hdr.graph.first_time_offset/1000;
@@ -34,10 +38,10 @@ clear x
 
 timeMarkerBioimpedance = unique(timeMarkerBioimpedance);
 
-%%
+
 Fs = 200;
 dt = 1/Fs;
-totalTime = 15; % seconds
+totalTime = 6.5; % seconds
 offset = 0.75;
 time = offset:dt:totalTime;
 
@@ -60,7 +64,7 @@ for indMarker = 1:size(timeMarkerBioimpedance,2)-1;
     tBegin = timeMarkerBioimpedanceInd(indMarker)+Fs*offset;
     tEnd = timeMarkerBioimpedanceInd(indMarker)+Fs*totalTime;
     
-    % Signal during the time of interest (manuever)
+    % Signal during the time of interest (maneuver)
     for indDataType = 1:size(legData,2)
         maxOverall(indMarker,indDataType) = ...
             max(legData(tBegin:tEnd,indDataType));
@@ -68,15 +72,23 @@ for indMarker = 1:size(timeMarkerBioimpedance,2)-1;
             min(legData(tBegin:tEnd,indDataType));
         dZOverall(indMarker,indDataType) = ...
             maxOverall(indMarker,indDataType) - minOverall(indMarker,indDataType);
+        
+        NAMES{indMarker,1} = acq.markers.szText{1,indMarker+1}(12:end);
     end
+%     open NAMES
+%     open dZOverall
     
-    acq.markers.szText{1,indMarker+1}(12:end)
+%     acq.markers.szText{1,indMarker+1}(12:end)
     
-    
+%     plot(legData(tBegin:tEnd,3))
+%     
 %     figure, plot(time', legROI,time(indMaxLeg),maximaLeg,'g*',time(indMinL),legROI(indMinL),'ro')
 %     title(horzcat('Leg ',acq.markers.szText{1,indMarker+1}(12:end)));
 %     xlabel('Time [s]');
 %     ylabel('Impedance [\Omega]');
     
 end
+
+% plot(time,bLegSMOOTH(tBegin:tEnd))
+
 
