ef98416164888070c2508cafff3cc43d59e00e99
Barry Belmont <belmont@umich.edu>
2014-10-16 12:43:22 -0400

    A quick and dirty error detector
    In case there are no markers to discover, return a NaN value and tell
    the user.

--- DRIVE Analysis/getACQmarkers.m
+++ DRIVE Analysis/getACQmarkers.m
@@ -1,31 +1,35 @@
-function [timeMarkerBioimpedance] = getACQmarkers(path, file)
+function [ACQmarkersInd] = getACQmarkers(path, file)
 %UNTITLED5 Summary of this function goes here
 %   Detailed explanation goes here
 
 acq = load_acq(strcat(path,file));
-timeStartBioimpedance = acq.hdr.graph.first_time_offset/1000;
-
-day = 60*60*24;
-timeStartBioimpedance = str2double(datestr(timeStartBioimpedance/day,'HHMMSS'));
-
-shift = 0;
-% Find markers in ACQKnowledge data
-timeMarkerBioimpedance = zeros(1,length(acq.markers.lSample));
-timeMarkerBioimpedanceInd = zeros(1,length(acq.markers.lSample));
-for nMarkers = 1:length(acq.markers.lSample)
-    if acq.markers.lSample(nMarkers) == 0
-        shift = shift + 1;
-    else
-        x = str2double(datestr(timeStartBioimpedance/day+...
-            (double(acq.markers.lSample(nMarkers)/200)/day),'HHMMSS'));
-        timeMarkerBioimpedance(nMarkers-shift) = x;
-%         clipName{1,nMarkers-shift} = acq.markers.szText{1,nMarkers}(11:end);
-        timeMarkerBioimpedanceInd(nMarkers-shift) = acq.markers.lSample(nMarkers);
+if isfield(acq.markers,'lMarkers') == 1
+    timeStartACQ = acq.hdr.graph.first_time_offset/1000;
+    
+    day = 60*60*24;
+    timeStartACQ = str2double(datestr(timeStartACQ/day,'HHMMSS'));
+    
+    shift = 0;
+    % Find markers in ACQKnowledge data
+    ACQmarkers = zeros(1,length(acq.markers.lSample));
+    ACQmarkersInd = zeros(1,length(acq.markers.lSample));
+    for nMarkers = 1:length(acq.markers.lSample)
+        if acq.markers.lSample(nMarkers) == 0
+            shift = shift + 1;
+        else
+            x = str2double(datestr(timeStartACQ/day+...
+                (double(acq.markers.lSample(nMarkers)/200)/day),'HHMMSS'));
+            ACQmarkers(nMarkers-shift) = x;
+            ACQmarkersInd(nMarkers-shift) = acq.markers.lSample(nMarkers);
+        end
     end
-end
-clear x
-
-timeMarkerBioimpedance = unique(timeMarkerBioimpedance);
-
+    clear x
+    
+    ACQmarkersInd = unique(ACQmarkersInd);
+    
+else
+    fprint('There are no markers to display')
+    ACQmarkers = NaN;
+    
 end
 
