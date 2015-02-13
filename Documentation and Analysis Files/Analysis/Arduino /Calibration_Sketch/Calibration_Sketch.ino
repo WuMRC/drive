8143f80d96db7fda14c00ca3309b1a6b58a7ee33
Adetunji Dahunsi <tjdah100@gmail.com>
2015-02-13 09:28:37 -0500

    Added skeletons for MATLAB calibration objects
    Also added GitHub to my MATLAB path so other test scripts will find
    their way here.

---
+++ Documentation and Analysis Files/Analysis/Arduino /Calibration_Sketch/Calibration_Sketch.ino
@@ -0,0 +1,7 @@
+// AD5933 library implementation via Arduino serial monitor by Adetunji Dahunsi <tunjid.com>
+// Updates should (hopefully) always be available at https://github.com/WuMRC
+
+// This sketch prints out constituents for a MATLAB Map object "impedanceMap".
+// Values of [f,r1,r2,c,key] are mapped to [z,r,x].
+
+
