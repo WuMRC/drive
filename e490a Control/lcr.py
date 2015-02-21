9c08667711029da448a363799f8794a8e1e7db8c
Adetunji Dahunsi <tjdah100@gmail.com>
2015-02-21 17:48:42 -0500

    Added command to fetch LCR data

--- e490a Control/lcr.py
+++ e490a Control/lcr.py
@@ -1,9 +1,10 @@
 # Import USBTMC class
 import usbtmc
-# Set lcr to class instance
+# Set lcr to class instance {Intrument(VendorID, ProductID)}
 lcr =  usbtmc.Instrument(2391, 2313)
 # Print identification to make sure device is connected
 print(lcr.ask("*IDN?"))
 # Trigger LCR
 lcr.write(":TRIG:IMM")
 # Read data from LCR
+print(lcr.ask("FETC:IMP:FORM?"))
